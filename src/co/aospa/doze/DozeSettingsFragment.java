a7efe249e6b67d2c103de18e28245cbdcba4a0bb
notakbuzz <bistodroid@gmail.com>
2024-11-01 09:04:39 +0100

    ParanoidDoze: Migrate to CompoundButtonOCL and resources
    Change-Id: I4222f0445cef183285c988e73b86259cf9a65b41

--- src/co/aospa/doze/DozeSettingsFragment.java
+++ src/co/aospa/doze/DozeSettingsFragment.java
@@ -37,12 +37,13 @@ import androidx.preference.PreferenceFragment;
 import androidx.preference.SwitchPreference;
 
 import com.android.settingslib.widget.MainSwitchPreference;
-import com.android.settingslib.widget.OnMainSwitchChangeListener;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 
 import co.aospa.doze.R;
 
 public class DozeSettingsFragment extends PreferenceFragment implements OnPreferenceChangeListener,
-        OnMainSwitchChangeListener {
+        OnCheckedChangeListener  {
 
     private MainSwitchPreference mSwitchBar;
 
@@ -135,7 +136,7 @@ public class DozeSettingsFragment extends PreferenceFragment implements OnPrefer
     }
 
     @Override
-    public void onSwitchChanged(Switch switchView, boolean isChecked) {
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)  {
         DozeUtils.enableDoze(getActivity(), isChecked);
         DozeUtils.checkDozeService(getActivity());
 
